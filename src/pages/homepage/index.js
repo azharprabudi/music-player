cb8fb1762b653909f49c83f4a5baffc8a8c1bd7b
azharprabudi <azharprabui@gmail.com>
2019-04-03 16:26:35 +0700

    create homepage

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,10 +1,36 @@
 import React, { PureComponent } from "react";
+import "./homepage.css";
 
 class Homepage extends PureComponent {
+  onSubmit = e => {
+    e.preventDefault();
+  };
+
+  searchEntityMusic = e => {
+    const { value: search } = e.target;
+  };
+
   render() {
     return (
-      <div>
-        <h1>helo</h1>
+      <div className="container">
+        <div className="background-image">
+          <img
+            alt={"music"}
+            src={`${process.env.PUBLIC_URL}/assets/images/bg.jpg`}
+            className="full-image"
+          />
+        </div>
+        <form onSubmit={this.onSubmit}>
+          <div className="content">
+            <h1>Music Collection</h1>
+            <input
+              type="text"
+              className="search-music"
+              placeholder="Type for search artists, albums, tracks at here ..."
+              onChange={this.onChange}
+            />
+          </div>
+        </form>
       </div>
     );
   }
