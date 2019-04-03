b2ca6381ea19aefdb2c9c6e9a29738e911979994
azharprabudi <azharprabui@gmail.com>
2019-04-03 13:41:45 +0700

    remove unused file

---
+++ src/homepage/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class Homepage extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>helo</h1>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
