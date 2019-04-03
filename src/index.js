3031c30e51904414375d04ada55e2bc3f98ebda4
azharprabudi <azharprabui@gmail.com>
2019-04-03 13:55:26 +0700

    implement resolve module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Homepage from "./homepage";
+import Homepage from "pages/homepage";
 
 ReactDOM.render(<Homepage />, document.getElementById("root"));
